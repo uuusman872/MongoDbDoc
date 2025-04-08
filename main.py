9f8c15d9d8296ccda5180e34881a0bce3454a1a7
Muhammad-Usman <muhammad.usman@theowlsense.com>
2025-04-08 10:08:58 +0500

    MongoDb Documentation

---
+++ main.py
@@ -0,0 +1,9 @@
+import pymongo
+
+if __name__ == "__main__":
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    print(client)
+    db = client['harry']
+    collection = db["mySampleCollectionForHarry"]
+
+    collection.insert_one({"hello": "world"})
